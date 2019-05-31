16225f987d3ba15818b5035ed5706554441ca9bb
usamanadeemrm7 <35189211+usamanadeemrm7@users.noreply.github.com>
2019-05-31 17:27:54 +0500

    Add files via upload

---
+++ add.blade.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>VSB</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+        
+            html, body {
+                background-image:  url("http://timesofkabul.com/wp-content/uploads/2016/08/Pakistan-flag-HD-Images-Wallpapers-Pics-5.jpg");
+                
+                background-size: cover;
+    background-attachment: scroll;
+                color: #000;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                color: #fff;
+                text-align: center;
+                background-color: rgba(0,0,0,0.5);
+               
+                
+
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #fff;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @if (Auth::check())
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                       
+                    @endif
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Candidate Added!
+                </div>
+
+                <div class="links">
+                    <a href="{{ url ('/create')}}">ADD More</a>
+                    <a href="{{ url ('/')}}">Home</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ castvote.blade.php
@@ -0,0 +1,607 @@
+<!DOCTYPE HTML>
+<html lang="zxx">
+
+<head>
+    <title>VSB Registeration Form</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    
+    <script>
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+    
+    <link rel="stylesheet" href="css/fontawesome-all.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+    <link href="//fonts.googleapis.com/css?family=Encode+Sans+Condensed:100,200,300,400,500,600,700,800,900&amp;subset=latin-ext,vietnamese"
+        rel="stylesheet">
+    
+</head>
+
+<body>
+    <style>
+
+body {
+    background-image:  url("http://timesofkabul.com/wp-content/uploads/2016/08/Pakistan-flag-HD-Images-Wallpapers-Pics-5.jpg");
+                
+                background-size: cover;
+    background-attachment: scroll;
+    font-size: 100%;
+    font-family: 'Encode Sans Condensed', sans-serif;
+    overflow-x: hidden;
+}
+
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+dl,
+dt,
+dd,
+ol,
+nav ul,
+nav li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+}
+
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section {
+    display: block;
+}
+
+ol,
+ul {
+    list-style: none;
+    margin: 0px;
+    padding: 0px;
+}
+
+blockquote,
+q {
+    quotes: none;
+}
+
+blockquote:before,
+blockquote:after,
+q:before,
+q:after {
+    content: '';
+    content: none;
+}
+
+table {
+    border-collapse: collapse;
+    border-spacing: 0;
+}
+
+
+
+a {
+    text-decoration: none;
+}
+
+.txt-rt {
+    text-align: right;
+}
+
+
+
+.txt-lt {
+    text-align: left;
+}
+
+
+.txt-center {
+    text-align: center;
+}
+
+
+
+.float-rt {
+    float: right;
+}
+
+
+.float-lt {
+    float: left;
+}
+
+
+
+.clear {
+    clear: both;
+}
+
+
+
+.pos-relative {
+    position: relative;
+}
+
+
+
+.pos-absolute {
+    position: absolute;
+}
+
+
+
+.vertical-base {
+    vertical-align: baseline;
+}
+
+.vertical-top {
+    vertical-align: top;
+}
+
+
+
+nav.vertical ul li {
+    display: block;
+}
+
+
+
+nav.horizontal ul li {
+    display: inline-block;
+}
+
+
+
+img {
+    max-width: 100%;
+}
+
+
+
+
+
+h1 {
+    font-family: Century Gothic;
+    font-size: 3.5em;
+    color: #fff;
+    letter-spacing: 2px;
+    text-align: center;
+    text-shadow: 1px 2px 1px rgba(68, 68, 68, 0.37);
+    margin: 3vw 1vw;
+    font-weight: 600;
+}
+
+h1 span {
+    color: #f13f57;
+}
+
+
+.sub-main-w3 {
+    display: -webkit-flex;
+    display: -webkit-box;
+    display: -moz-flex;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: flex;
+    align-items: center;
+    -webkit-box-pack: center;
+    -moz-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+}
+
+.sub-main-w3 form {
+    max-width: 600px;
+    margin: 0 5vw;
+    background: linear-gradient(to right, rgba(245, 66, 157, 0.61), rgba(165, 203, 251, 0.62));
+    background: rgba(6, 6, 6, 0.42);
+    padding: 3.5vw;
+    box-sizing: border-box;
+    display: -webkit-flex;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    box-shadow: 3px 6px 12px 1px rgba(0, 0, 0, 0.24);
+}
+
+.form-style-agile {
+    margin-bottom: 1.5em;
+    flex-basis: 100%;
+    -webkit-flex-basis: 100%;
+    text-align: left;
+
+}
+
+.sub-main-w3 label {
+    font-size: 14px;
+    color: #fff;
+    display: inline-block;
+    font-weight: 500;
+    margin-bottom: 12px;
+    text-transform: capitalize;
+    letter-spacing: 1px;
+    text-align: left;
+}
+
+.sub-main-w3 label i {
+    font-size: 15px;
+    margin-right: 5px;
+    color: #f76b53;
+    line-height: 1.9;
+    text-align: left;
+}
+
+.form-style-agile input[type="text"], .form-style-agile input[type="password"] {
+    width: 100%;
+    color: #000;
+    outline: none;
+    font-size: 14px;
+    letter-spacing: 1px;
+    padding: 10px 10px;
+    box-sizing: border-box;
+    border: none;
+    background: #fff;
+    border-bottom: 3px double green;
+    border-radius: 8px;
+}
+
+.sub-main-w3 input[type="submit"] {
+    color: #fff;
+    background: #000;
+    border: none;
+    padding: 14px 0;
+    margin-top: 30px;
+    outline: none;
+    width: 32%;
+    border-radius: 8px;
+    font-size: 16px;
+    cursor: pointer;
+    letter-spacing: 2px;
+    -webkit-transition: 0.5s all;
+    -o-transition: 0.5s all;
+    -moz-transition: 0.5s all;
+    -ms-transition: 0.5s all;
+    transition: 0.5s all;
+    box-shadow: 2px 4px 13px 1px rgba(0, 0, 0, 0.42);
+}
+
+.sub-main-w3 input[type="submit"]:hover {
+    opacity:.7;
+    -webkit-transition: 0.5s all;
+    -o-transition: 0.5s all;
+    -moz-transition: 0.5s all;
+    -ms-transition: 0.5s all;
+    transition: 0.5s all;
+}
+
+
+.container {
+    position: relative;
+}
+
+.bottom {
+    width: 100%;
+    margin: 0 auto;
+    text-align: center;
+    padding: 10px 0;
+    height: 100px;
+    position: absolute;
+}
+
+.bottom h3 {
+    color: white;
+    font-size: 30px;
+    font-weight: bold;
+    margin-top: 45px;
+    padding-bottom: 45px;
+}
+.form-radio {
+  margin-bottom: 18px; }
+  .form-radio input {
+    width: auto;
+    display: inline-block; }
+
+.radio-label {
+  padding-right: 72px; }
+
+.form-radio-item {
+  position: relative;
+  margin-right: 45px; }
+  .form-radio-item label {
+    font-weight: 500;
+    font-size: 13px;
+    padding-left: 25px;
+    position: relative;
+    z-index: 9;
+    display: block;
+    cursor: pointer; }
+
+
+.blue {
+    color: #09c;
+}
+
+
+
+.footer {
+    margin: 12em .5em 2em;
+    margin-top: 480px;
+}
+
+.footer h2 {
+    font-size: 15px;
+    color: black;
+    letter-spacing: 2px;
+    text-align: center;
+    line-height: 1.8;
+}
+
+.footer h2 a {
+    color: #e20303;
+    -webkit-transition: 0.5s all;
+    -o-transition: 0.5s all;
+    -moz-transition: 0.5s all;
+    -ms-transition: 0.5s all;
+    transition: 0.5s all;
+}
+
+.footer h2 a:hover {
+    color: #fff;
+    -webkit-transition: 0.5s all;
+    -o-transition: 0.5s all;
+    -moz-transition: 0.5s all;
+    -ms-transition: 0.5s all;
+    transition: 0.5s all;
+}
+
+
+
+@media(max-width:1920px) {
+    h1 {
+        font-size: 4vw;
+    }
+}
+
+@media(max-width:1024px) {
+    h1 {
+        font-size: 4.5vw;
+
+    }
+}
+
+@media(max-width:900px) {
+    .w3ls-social {
+        left: 6%;
+    }
+}
+
+@media(max-width:800px) {
+    h1 {
+        font-size: 2.6em;
+    }
+    .sub-main-w3 {
+        float: none;
+        text-align: left;
+    }
+    .w3ls-social {
+        position: static;
+        margin-top: 5em;
+    }
+    
+}
+
+    
+}
+
+@media(max-width:480px) {
+    h1 {
+        font-size: 2.3em;
+    }
+    .sub-main-w3 form {
+        padding: 7.5vw;
+    }
+    
+
+}
+.form-select {
+  position: relative; }
+  h4{
+    font-size: 15px;
+    font-family: Century Gothic;
+    color: red;
+    letter-spacing: 2px;
+    text-align: center;
+    text-shadow: 1px 2px 1px rgba(68, 68, 68, 0.37);
+    margin: 3vw 1vw;
+    font-weight: 600;
+  }
+
+select {
+  display: block;
+  width: 100%;
+  border: 1px solid #ebebeb;
+  box-sizing: border-box;
+  font-family: 'Montserrat';
+  font-weight: 500;
+    outline: none;
+    font-size: 14px;
+    letter-spacing: 1px;
+    padding: 15px 15px;
+    background: #fff;
+    border-bottom: 3px double green;
+    border-radius: 8px;
+    box-sizing: 10px;
+   }
+  select:focus {
+    border: 1px solid #ff6801; }
+
+
+@media(max-width:320px) {
+    h1 {
+        font-size: 1.8em;
+    }
+    .sub-main-w3 form {
+        padding: 25px 14px;
+    }
+    
+}
+
+
+</style>
+    
+    <h1>
+        Cast Vote
+    </h1>
+
+    <h4>
+                             @if(count($errors)>0)
+<ul>
+    @foreach($errors->all() as $error)
+    <li class= "alert alert-danger">{{$error}} </li>
+    @endforeach
+</ul>
+@endif
+    </h4>
+
+
+
+
+    
+    <div class="container">
+
+    
+
+        
+        <div class="sub-main-w3">
+
+            <form action="calculate" method="GET">
+            {{  csrf_field()  }}
+
+             <div class="form-style-agile">
+                    <label>
+                        <i class="fas fa-unlock-alt"></i>
+                        Enter Your CNIC to cast vote!
+
+                    </label>
+                    
+                </div>
+                <div class="form-style-agile">
+                    <label>
+                        <i class="fas fa-unlock-alt"></i>
+                        Voter's CNIC
+
+                    </label>
+                    <input placeholder="CNIC" name="CNIC" type="text" required="">
+                </div>
+                
+
+    
+              
+
+                  
+                
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+        
+    </div>
+    
+</body>
+
+</html>
